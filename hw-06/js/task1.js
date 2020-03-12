aec83d0680bfbade31c0739e903b24cef0015b3d
Anna Pustovit <pustovit.anna90@gmail.com>
2020-03-12 16:54:45 +0200

    added HM-6

---
+++ hw-06/js/task1.js
@@ -0,0 +1,120 @@
+'use strict';
+
+console.log('Task 1');
+
+// Задание 1
+// Получить массив имен всех пользователей (поле name).
+
+import users from './users.js';
+
+const getUserNames = users => {
+  // твой код
+  return users.map(users => users.name);
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+console.log('Task 2');
+
+// Задание 2
+// Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  return users.filter(user => user.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+// const activeUsers = users.filter(user => user.isActive);
+// console.log(activeUsers);
+
+console.log('Task 3');
+
+// Получить массив имен пользователей по полу (поле gender).
+
+const getUsersWithGender = (users, gender) => {
+  // твой код
+  return users.filter(user => user.gender === gender);
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+console.log('Task 4');
+
+// Получить массив только неактивных пользователей (поле isActive).
+
+const getInactiveUsers = users => {
+  // твой код
+  return users.filter(user => !user.isActive);
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+console.log('Task 5');
+// Задание 5
+// Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) => {
+  // твой код
+  return users.find(x => x.email === email);
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+console.log('Task 6');
+
+// Получить массив пользователей попадающих в возрастную категорию от min до max лет (поле age).
+
+const getUsersWithAge = (users, min, max) => {
+  // твой код
+  return users.filter(user => user.age > min && user.age < max);
+};
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
+
+console.log('Task 7');
+// Задание 7
+// Получить общую сумму баланса (поле balance) всех пользователей.
+
+const calculateTotalBalance = users =>
+  // твой код
+  users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
+
+console.log(calculateTotalBalance(users)); // 20916
+
+console.log('Task 8');
+// Массив имен всех пользователей у которых есть друг с указанным именем.
+
+const getUsersWithFriend = (users, friendName) => {
+  return users
+    .filter(user => user.friends.indexOf(friendName) !== -1)
+    .map(user => user.name);
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+
+console.log('Task 9');
+// Задание 9
+// Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (поле friends)
+
+const getNamesSortedByFriendsCount = users => {
+  return users.sort((a, b) => a.friends.length - b.friends.length);
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+console.log('Task 10');
+
+const getSortedUniqueSkills = users => {
+  return new Set(users.flatMap(user => user.skills).sort());
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
